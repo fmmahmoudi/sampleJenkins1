0baf1edb94119cacfb4aa79b644d1c3d96f06465
fami <you@example.com>
2025-11-16 18:42:55 +0330

    first commit

---
+++ src/main/java/com/example/project2/Project2Application.java
@@ -0,0 +1,17 @@
+package com.example.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project2Application.class, args);
+    }
+
+}
+
+//    git remote add origin https://github.com/fmmahmoudi/sampleJenkins1.git
+//        git branch -M main
+//        git push -u origin main
